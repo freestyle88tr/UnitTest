276d922f6ac9172469a10159f00169adcefb0ba8
user <usernotused@notusableemailadres.com>
2015-03-24 14:20:10 +0100

    First commit :)

---
+++ Calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int add(int a, int b)
+        {
+            return (a + b);
+        }
+      
+        public int subtract(int a, int b)
+        {
+            return (a - b);
+        }
+
+        public int multiply(int a, int b)
+        {
+            return (a * b);
+        }
+
+        public int divide(int a, int b)
+        {
+            return (a / b);
+        }
+    }
+}
---
+++ Calculator_UnitTest/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace Calculator_UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void add_test()
+        {
+            var test_object = new Program();
+            int expected = 5;
+            int actual = test_object.add(2, 3);
+            Assert.AreEqual<int>(expected, actual, "Did not get the right value from add method");
+        }
+        [TestMethod]
+        public void subtract_test()
+        {
+            var test_object = new Program();
+            int expected = 3;
+            int actual = test_object.subtract(6, 3);
+            Assert.AreEqual<int>(expected, actual, "Did not get the right value from subtract method");
+        }
+        [TestMethod]
+        public void multiply_test()
+        {
+            var test_object = new Program();
+            int expected = 15;
+            int actual = test_object.multiply(5, 3);
+            Assert.AreEqual<int>(expected, actual, "Did not get the right value from multiply method");
+        }
+        [TestMethod]
+        public void divide_test()
+        {
+            var test_object = new Program();
+            int expected = 3;
+            int actual = test_object.divide(9, 9);
+            Assert.AreEqual<int>(expected, actual, "Did not get the right value from divide method");
+        }
+    }
+}
