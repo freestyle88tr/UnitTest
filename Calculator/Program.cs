bd23ffc14b869764ab8bc08d4ff4e39b696ea3a8
user <usernotused@notusableemailadres.com>
2015-04-13 16:40:49 +0200

    Project with stylecop

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,30 +5,60 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Calculator
-{
+{   
+    /// <summary>
+    /// Class Program
+    /// </summary>
     public class Program // class program
     {
-
+        /// <summary>
+        /// Main program
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
 
         }
 
+        /// <summary>
+        /// add function
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int add(int a, int b)
         {
             return (a + b);
         }
       
+        /// <summary>
+        /// subtract function
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int subtract(int a, int b)
         {
             return (a - b);
         }
 
+        /// <summary>
+        /// multiply function
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int multiply(int a, int b)
         {
             return (a * b);
         }
 
+        /// <summary>
+        /// divide function
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int divide(int a, int b)
         {
             return (a / b);
