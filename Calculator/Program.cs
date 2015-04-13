a03dfc408f0220f7d8a1a8ee678b73499af5f78f
user <usernotused@notusableemailadres.com>
2015-04-13 19:54:38 +0200

    stylecop inside

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,29 +6,59 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
+    /// <summary>
+    /// Class
+    /// </summary>
     public class Program // class program
     {
-
+        /// <summary>
+        /// Main function
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
 
         }
 
+        /// <summary>
+        /// Add function
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int add(int a, int b)
         {
             return (a + b);
         }
       
+        /// <summary>
+        /// Subtract function
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int subtract(int a, int b)
         {
             return (a - b);
         }
 
+        /// <summary>
+        /// Multiply function
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int multiply(int a, int b)
         {
             return (a * b);
         }
 
+        /// <summary>
+        /// Divide function
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int divide(int a, int b)
         {
             return (a / b);
